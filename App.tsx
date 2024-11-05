9e74b6dd0784fd07ec54fe83d0d71079b5a24957
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-05 16:12:20 +0530

    Add app file

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Usha</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
