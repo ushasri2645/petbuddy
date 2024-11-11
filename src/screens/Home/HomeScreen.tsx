0a5ff9b9fed074b2ec949f9f0ec20e1fe2a4c00e
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-11 15:15:57 +0530

    Update home screen file

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
-const Home = ({navigation}:{navigation:any}) => {
+const Home = ({navigation}: {navigation: any}) => {
   return (
     <View style={styles.container}>
       <View style={styles.headerSection}>
         <Text style={styles.pawEmoji}>🐾</Text>
         <Text style={styles.headerText}>My Pets</Text>
+      </View>
+      <View style={styles.middleSection}></View>
+      <View style={styles.bottomSection}>
+        <View style={styles.addPet}>
+          <Image
+            style={styles.image}
+            source={require('../../../public/assets/Login/paw.png')}
+          />
+          <Text style={styles.addPetText}>Add Pet</Text>
+        </View>
       </View>
     </View>
   );
@@ -15,32 +25,60 @@ const Home = ({navigation}:{navigation:any}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin:10,
+    margin: 10,
+    // alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 20,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  headerSection: {
+    flexDirection: 'row',
+    gap: 10,
+    backgroundColor: 'white',
+    width: '95%',
+    height: '7%',
     alignItems: 'center',
-  },
-  headerText:{
-    fontSize:20,
-    color:'black',
-    fontWeight:'bold'
-  },
-  headerSection:{
-    flexDirection:'row',
-    gap:10,
-    backgroundColor:'white',
-    width:'95%',
-    height:'7%',
-    alignItems:'center',
-    padding:10,
-    borderRadius:22,
+    padding: 10,
+    borderRadius: 22,
     shadowColor: 'gray',
     shadowOpacity: 0.3,
     shadowRadius: 3.84,
-    elevation:4
-
+    elevation: 4,
   },
-  pawEmoji:{
-    color:'black',
-    fontSize:20
+  pawEmoji: {
+    color: 'black',
+    fontSize: 20,
+  },
+  bottomSection: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+
+    alignSelf: 'flex-end',
+    height: '20%',
+  },
+  image: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+  },
+  addPet: {
+    flexDirection: 'row',
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 20,
+    gap: 10,
+    alignItems: 'center',
+    
+  },
+  addPetText:{
+    color:'white'
+  },
+  middleSection:{
+
   }
 });
 
