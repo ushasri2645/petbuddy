96d21a4fb3b42367757ee4d45b1de7a49b4d5e70
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-14 01:46:20 +0530

    Update navigation tab images

--- Navigation.tsx
+++ Navigation.tsx
@@ -11,17 +11,26 @@ import Training from "./src/tabs/Training/TrainingTab";
 import Activity from "./src/screens/Activity/ActivityScreen";
 import Reminders from "./src/screens/Reminders/ReminderScreen";
 import PetScreen from "./src/screens/PetScreen/PetScreen";
+import Profile from "./src/screens/Profile/ProfileScreen";
+import GalleryScreen from "./src/screens/Gallery/GalleryScreen";
+import { Image, View } from "react-native";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export function StackNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: 'forestgreen' }, 
+      headerTintColor: '#fff',                   
+    }}>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Pet" component={PetScreen} />
       <Stack.Screen name="Activity" component={Activity}/>
       <Stack.Screen name="Reminders" component={Reminders}/>
+      <Stack.Screen name="Profile" component={Profile}/>
+      <Stack.Screen name="Gallery" component={GalleryScreen}/>
     </Stack.Navigator>
   );
 }
@@ -30,10 +39,47 @@ export function TabNavigator(): React.JSX.Element {
   return (
     <Tab.Navigator
       initialRouteName={"Home"}
+     screenOptions={({ route }) => ({       
+        headerShown: false ,           
+        tabBarIcon: ({ focused, color, size }) => {
+            let iconSource;
+            switch (route.name) {
+                case 'Home':
+                    iconSource = require('./public/assets/Tabs/homefilled.png')
+                    break;
+                case 'Services':
+                    iconSource =  require('./public/assets/Tabs/services.jpg');
+                    break;
+                case 'Training':
+                    iconSource = require('./public/assets/Tabs/traini.png')
+                    break;
+            }
+            return (
+                <View style={[focused ? { alignItems: 'center', backgroundColor:'darkgreen', width:50, borderRadius:10 }: { alignItems: 'center' }]}>
+                    <Image
+                        source={iconSource}
+                        style={{ width: 30, height: 30, borderRadius:20 }}
+                        resizeMode="cover"
+                    />
+                </View>
+            );
+        },
+        tabBarLabel: route.name, 
+        tabBarStyle: {
+            backgroundColor: 'green', 
+            height: 60,
+            paddingBottom: 5,
+        },
+        tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: 'bold',
+            color: 'white', 
+        },
+      })}
     >
       <Tab.Screen name="Home" component={StackNavigator} options={{ headerShown: false }}/>
       <Tab.Screen name="Services" component={Services}  />
-      <Tab.Screen name="Taining" component={Training}  />
+      <Tab.Screen name="Training" component={Training}  />
     </Tab.Navigator>
   );
 }
