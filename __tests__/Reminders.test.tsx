72da3984f128e02d7d72ad0c545d1a920119142d
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-14 01:38:04 +0530

    Update route for reminder tests

--- __tests__/Reminders.test.tsx
+++ __tests__/Reminders.test.tsx
@@ -2,8 +2,10 @@ import { render } from "@testing-library/react-native";
 import Reminders from "../src/screens/Reminders/ReminderScreen";
 
 describe("Test for remidners page",()=>{
+    const mockRoute = { params: { pet: { name: 'Buddy', age: 3, weight: 15, height: 50, color: 'Brown', remarks: 'Friendly dog' } } };
+
     it("Should render reminder text",()=>{
-        const {getByTestId} = render(<Reminders/>)
+        const {getByTestId} = render(<Reminders route={mockRoute} />)
         expect(getByTestId("reminder-header")).toBeTruthy()
         expect(getByTestId("+-icon")).toBeTruthy()
     })
